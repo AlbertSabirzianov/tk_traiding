42edcfb6b5e0192959c5dca3239f9532968ff368
AlbertSabirzianov <albertuno@mail.ru>
2025-04-02 15:55:07 +0300

    добавил новые функции брокерра тинькофф

--- src/app/tinkoff_service.py
+++ src/app/tinkoff_service.py
@@ -1,10 +1,27 @@
+import datetime
 import functools
+from decimal import Decimal
+from pprint import pprint
 
 from pandas import DataFrame
-from tinkoff.invest import Account, PortfolioResponse, PositionsResponse, GetOrdersResponse
-from tinkoff.invest import Client, SecurityTradingStatus
+from tinkoff.invest import Account, PortfolioResponse, PositionsResponse, GetOrdersResponse, OperationsResponse
+from tinkoff.invest import Client, SecurityTradingStatus, MoneyValue
 from tinkoff.invest.constants import INVEST_GRPC_API, INVEST_GRPC_API_SANDBOX
-from tinkoff.invest.utils import quotation_to_decimal
+from tinkoff.invest.schemas import OrderDirection, OrderType
+from tinkoff.invest.utils import quotation_to_decimal, decimal_to_quotation
+
+
+class TkBroker:
+
+    def __init__(self, tok: str, target: str):
+        assert target in (INVEST_GRPC_API, INVEST_GRPC_API_SANDBOX)
+        self.token: str = tok
+        self.target: str = target
+        self.shares_df: DataFrame = get_instruments_df(self.token)
+
+
+    # @property
+    # def money
 
 
 @functools.lru_cache
@@ -59,9 +76,51 @@ def get_positions(token: str,  target: str = INVEST_GRPC_API) -> PositionsRespon
 
 
 def get_orders(token: str, target: str = INVEST_GRPC_API) -> GetOrdersResponse:
-    with Client(token=token) as client:
+    with Client(token=token, target=target) as client:
         return client.orders.get_orders(account_id=get_account(token, target).id)
 
 
+def byu(figi: str, token: str, target: str = INVEST_GRPC_API):
+    with Client(token=token, target=target) as client:
+        sc = client.orders.post_order(
+            figi=figi,
+            quantity=1,
+            direction=OrderDirection.ORDER_DIRECTION_BUY,
+            account_id=get_account(token, target).id,
+            order_type=OrderType.ORDER_TYPE_MARKET,
+        )
+        print(f"Return {sc}")
 
+
+def sell(figi: str, token: str, target: str = INVEST_GRPC_API):
+    with Client(token=token, target=target) as client:
+        client.orders.post_order(
+            figi=figi,
+            quantity=1,
+            direction=OrderDirection.ORDER_DIRECTION_SELL,
+            account_id=get_account(token, target).id,
+            order_type=OrderType.ORDER_TYPE_MARKET,
+        )
+
+
+def get_operations(token: str, target: str = INVEST_GRPC_API) -> OperationsResponse:
+    with Client(token=token, target=target) as client:
+        return client.operations.get_operations(
+            account_id=get_account(token, target).id,
+            from_=datetime.datetime.now().replace(hour=0, minute=0, second=0),
+            to=datetime.datetime.now()
+        )
+
+
+def add_money_sandbox(token: str, money: float):
+    with Client(token=token, target=INVEST_GRPC_API_SANDBOX) as client:
+        money = decimal_to_quotation(Decimal(money))
+        client.sandbox.sandbox_pay_in(
+            account_id=get_account(token, INVEST_GRPC_API_SANDBOX).id,
+            amount=MoneyValue(units=money.units, nano=money.nano, currency="rub")
+        )
+
+
+if __name__ == "__main__":
+    pass
 
